47bde7e7372b925e7026f893b201878cdc5dbd0d
Chris <chris-masters@users.noreply.github.com>
2023-03-02 16:47:12 -0500

    trying install of modules to avoid manifest errors on required modules.

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,13 @@
 
 $modulePath = "$PSScriptRoot\CheckPoint"
 
+$requiredModules = @('core', 'Posh-SSH')
+
+foreach ($mod in $requiredModules) {
+    $module = Get-Module -Name $mod -ListAvailable
+    if ($module -eq $null) {
+        Install-Module -Name $mod -Force -Scope CurrentUser
+    }
+}
+
 Publish-Module -Path $modulePath -NuGetApiKey $Env:APIKEY
